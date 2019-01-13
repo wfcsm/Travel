f3afe25bcfbe951427077e8ee49da6a33c646886
zyc <627245048@qq.com>
2019-01-13 13:56:17 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,89 @@
+<template>
+    <div>
+        <div class="title">热销推荐</div>
+        <ul>
+            <li class="item border-bottom" v-for="item of recommends" :key="item.id">
+                <img class="item-img" :src="item.url" >        
+                <div class="item-info">
+                    <p class="item-title">{{ item.title}}</p>
+                    <p class="item-desc">{{ item.desc}}</p>
+                    <button class="item-button">查看详情</button>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeRecommend',
+    data() {
+        return {
+            recommends:[
+                {
+                    id: '0001',
+                    url: 'http://img1.qunarzz.com/sight/p0/201309/25/70edd368471094bdc8d65eac.jpg_200x200_75b6f391.jpg',
+                    title: '临安湍口众安氡温泉',
+                    desc: '夏凉冬温，隆冬暖薰蒸，垒垒上浮'
+                },{
+                    id: '0002',
+                    url: 'http://img1.qunarzz.com/sight/p0/201309/25/70edd368471094bdc8d65eac.jpg_200x200_75b6f391.jpg',
+                    title: '临安湍口众安氡温泉',
+                    desc: '夏凉冬温，隆冬暖薰蒸，垒垒上浮'
+                },{
+                    id: '0003',
+                    url: 'http://img1.qunarzz.com/sight/p0/201309/25/70edd368471094bdc8d65eac.jpg_200x200_75b6f391.jpg',
+                    title: '临安湍口众安氡温泉',
+                    desc: '夏凉冬温，隆冬暖薰蒸，垒垒上浮'
+                },{
+                    id: '0004',
+                    url: 'http://img1.qunarzz.com/sight/p0/201309/25/70edd368471094bdc8d65eac.jpg_200x200_75b6f391.jpg',
+                    title: '临安湍口众安氡温泉',
+                    desc: '夏凉冬温，隆冬暖薰蒸，垒垒上浮'
+                }
+                
+            ]
+        }
+    }
+
+    
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+.title
+    height : .8rem
+    line-height : .8rem
+    background: #eee
+    text-indent: .2rem
+    margin-top: .2rem
+.item
+    display flex 
+    height: 1.9rem
+    .item-img
+        width: 1.7rem
+        height: 1.7rem
+        padding : .1rem
+    .item-info
+        display 1
+        padding: .1rem
+        min-width: 0
+        .item-title
+            line-height: .54rem
+            font-size: .32rem
+            ellipsis()
+        .item-desc
+            line-height : .4rem
+            color: #ccc
+            ellipsis()
+        .item-button
+            margin-top: .16rem
+            background: #ff9300
+            padding: 0 .2rem
+            border-radius: .06rem
+            color: #fff
+            line-height: .44rem
+    
+</style>
+
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,82 @@
+<template>
+    <div>
+        <div class="title">周末去哪儿</div>
+        <ul>
+            <li class="item  border-bottom" v-for="item of recommends" :key="item.id">
+                <div class="item-img-wrapper">
+                    <img class="item-img" :src="item.url" >        
+                </div>
+                <div class="item-info">
+                    <p class="item-title">{{ item.title}}</p>
+                    <p class="item-desc">{{ item.desc}}</p>
+                    <!-- <button class="item-button">查看详情</button> -->
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeWeekend',
+    data() {
+        return {
+            recommends:[
+                {
+                    id: '0001',
+                    url: 'http://img1.qunarzz.com/sight/source/1510/c1/afe3ab32b3bc23.jpg_r_640x214_beb61760.jpg',
+                    title: '临安湍口众安氡温泉',
+                    desc: '夏凉冬温，隆冬暖薰蒸，垒垒上浮'
+                },{
+                    id: '0002',
+                    url: 'http://img1.qunarzz.com/sight/source/1510/c1/afe3ab32b3bc23.jpg_r_640x214_beb61760.jpg',
+                    title: '临安湍口众安氡温泉',
+                    desc: '夏凉冬温，隆冬暖薰蒸，垒垒上浮'
+                },{
+                    id: '0003',
+                    url: 'http://img1.qunarzz.com/sight/source/1510/c1/afe3ab32b3bc23.jpg_r_640x214_beb61760.jpg',
+                    title: '临安湍口众安氡温泉',
+                    desc: '夏凉冬温，隆冬暖薰蒸，垒垒上浮'
+                },{
+                    id: '0004',
+                    url: 'http://img1.qunarzz.com/sight/source/1510/c1/afe3ab32b3bc23.jpg_r_640x214_beb61760.jpg',
+                    title: '临安湍口众安氡温泉',
+                    desc: '夏凉冬温，隆冬暖薰蒸，垒垒上浮'
+                }
+                
+            ]
+        }
+    }
+
+    
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+.title
+    height : .8rem
+    line-height : .8rem
+    background: #eee
+    text-indent: .2rem
+    margin-top: .2rem
+.item-img-wrapper
+    height: 0
+    width: 100%
+    overflow hidden
+    padding-bottom: 32.9%
+    .item-img
+        width: 100%
+.item-info
+    padding: .1rem
+    min-width: 0
+    .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+    .item-desc
+        line-height : .4rem
+        color: #ccc
+        ellipsis()
+</style>
+
