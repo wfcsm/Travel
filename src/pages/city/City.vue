8227f0fa02c9c9858ceef9be09cfc09d9740a457
zyc <627245048@qq.com>
2019-01-13 19:50:38 +0800

    add city list

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,17 +2,23 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-list></city-list>
+        <city-alpabet></city-alpabet>
     </div>
 </template>
 
 <script>
 import CityHeader from './components/header.vue'
 import CitySearch from './components/search.vue'
+import CityList from './components/list.vue'
+import CityAlpabet from './components/Alphabet.vue'
 export default {
     name: 'City',
     components: {
         CityHeader,
-        CitySearch
+        CitySearch,
+        CityList,
+        CityAlpabet
     }
     
 }
---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,35 @@
+<template>
+    <ul class="list">
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+    </ul>
+</template>
+
+<script>
+export default {
+   name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/variables.styl'
+    .list
+        position absolute
+        top: 1.58rem
+        right: 0
+        bottom: 0
+        width: .4rem
+        display: flex
+        flex-direction:column
+        justify-content center
+        .item
+           line-height: .44rem
+           text-align: center
+           color: $bgColor 
+       
+</style>
