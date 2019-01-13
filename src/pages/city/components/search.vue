1281e60da99314c444168cda63b91287abcb0d56
zyc <627245048@qq.com>
2019-01-13 17:34:00 +0800

    add search to city

---
+++ src/pages/city/components/search.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="search">
+      <input type="text" placeholder="请输入您要搜索的地址" class="text-address">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/variables.styl'
+.search
+    background: $bgColor
+    height: .72rem
+    padding:0 .1rem
+    .text-address
+        width : 100%
+        height: .62rem
+        line-height: .62rem
+        text-align: center
+        border-radius: .1rem
+        box-sizing:border-box
+        padding: 0 .1rem
+        
+</style>
