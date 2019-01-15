9a2d3518011379997926774600398b0fe8a5a862
zyc <627245048@qq.com>
2019-01-15 20:20:58 +0800

    add keepalive

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app">
-    <router-view/>
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
-<script>
+<script> 
 export default {
   name: 'App'
 }
